8d03cb57569419d74573d89a2142869979601be9
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2023-07-13 11:00:28 +0200

    ClassyFire color-coding for biomolecules

--- umap_vis.py
+++ umap_vis.py
@@ -6,6 +6,28 @@ from dash import dcc, html, Input, Output, no_update, Dash, callback
 from rdkit.Chem import MolFromSmiles, Draw
 import os
 
+COLOR_MAPPING = {'Amino acids, peptides, and analogues': '#fe9929',
+                 'Peptides': '#cc4c02',
+                 'Fatty Acyls': '#fc9272',
+                 'Glycerolipids': '#ef3b2c',
+                 'Glycerophospholipids': '#99000d',
+                 'Cholines': '#67000d',
+                 'Amines': '#fff7bc',
+                 'Trialkylamines': '#fee391',
+                 'Sphingolipids': '#df65b0',
+                 'Organoheterocyclic compounds': '#dadaeb',
+                 'Benzenoids': '#6a51a3',
+                 'Diazines': '#3300cc',
+                 'Azoles': '#3f007d',
+                 'Steroids and steroid derivatives': '#c7e9c0',
+                 'Prenol lipids': '#41ab5d',
+                 'Triterpenoids': '#00441b',
+                 'Bile acids, alcohols and derivatives': '#02818a',
+                 'Phenylpropanoids and polyketides': '#a6cee3',
+                 'Carbohydrates and carbohydrate conjugates': '#fccde5',
+                 'Organosulfur compounds': '#ffea19',
+                 'Other': '#b6b6b6'}
+
 # for reverse proxy, add prefix to dash app settings
 dash_settings = {}
 try:
@@ -22,7 +44,6 @@ except Exception as e:
 app = Dash(__name__, **dash_settings)
 server = app.server
 
-
 umap_df = pd.read_csv('umap_df.csv')
 graph = dcc.Graph(id='umap-plot', clear_on_unhover=True, config=dict(scrollZoom=True))
 dropdown = dcc.Dropdown(umap_df.set.unique().tolist(), 'Tox21', id='set-select', clearable=False)
@@ -33,17 +54,27 @@ app.layout = html.Div([dropdown, graph, tooltip], style=dict(width='200px'))
 
 @app.callback(Output(graph, 'figure'), Input(dropdown, 'value'))
 def update_figure(dataset):
-    fig = px.scatter(umap_df.loc[umap_df.set.isin(['biomolecules', dataset])],
-                     x='umap1', y='umap2', color='set',
+    if (dataset == 'biomolecules'):
+        # special plot with ClassyFire classes colored
+        fig = px.scatter(umap_df.loc[umap_df.set == 'biomolecules'],
+                     x='umap1', y='umap2', color='classyfire_label',
                      custom_data=['smiles', 'name', 'classyfire_labels'],
                      template='simple_white',
-                     color_discrete_map={'biomolecules': '#aaaaaa'} |
-                     {s: '#fc054b' for s in umap_df.set.unique().tolist() if s != 'biomolecules'})
+                     color_discrete_map=COLOR_MAPPING)
+    else:
+        fig = px.scatter(umap_df.loc[umap_df.set.isin(['biomolecules', dataset])],
+                         x='umap1', y='umap2', color='set',
+                         custom_data=['smiles', 'name', 'classyfire_labels'],
+                         template='simple_white',
+                         color_discrete_map={'biomolecules': '#aaaaaa'} |
+                         {s: '#fc054b' for s in umap_df.set.unique().tolist() if s != 'biomolecules'})
     fig.update_traces(hoverinfo='none', hovertemplate=None)
     fig.update_traces(marker={'size': 2})
     fig.update_layout(
         dragmode='pan',
-        width=800, height=700,
+        width=970 if dataset == 'biomolecules' else 800, height=700,
+        legend={'itemsizing': 'constant'},
+        legend_title_text=None
     )
     return fig
 
